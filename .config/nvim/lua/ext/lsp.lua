20f1931e9f31cb26d7b4e72d186e5e5072088a24
Vitor Hugo Salgado <2922948+vitorsalgado@users.noreply.github.com>
2023-01-17 12:09:02 +0100

    fix nvim config

--- .config/nvim/lua/ext/lsp.lua
+++ .config/nvim/lua/ext/lsp.lua
@@ -3,7 +3,7 @@ require "lsp_signature".setup()
 vim.o.completeopt = 'menuone,noselect'
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local cmp = require 'cmp'
 local luasnip = require("luasnip")
